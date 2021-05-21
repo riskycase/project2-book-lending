d33b796d19ff2a0ab9e50178f09718655f195d2e
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2021-05-21 22:31:58 +0530

    Add a basic launch activity
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

---
+++ app/src/main/java/com/riskycase/twoVandaH/LaunchActivity.kt
@@ -0,0 +1,114 @@
+package com.riskycase.twoVandaH
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.SignInButton
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.ktx.Firebase
+
+class LaunchActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
+    private lateinit var db: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        setContentView(R.layout.activity_launch)
+
+        this.window.statusBarColor = getColor(R.color.launch_statusbar)
+        
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+        if(auth.currentUser != null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+        else
+            findViewById<SignInButton>(R.id.sign_in_button).visibility = SignInButton.VISIBLE
+
+        super.onCreate(savedInstanceState)
+
+        db = FirebaseFirestore.getInstance()
+
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        val signInButton = findViewById<SignInButton>(R.id.sign_in_button)
+
+        signInButton.setOnClickListener {
+            signIn()
+        }
+
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+
+                    // Create a new user with a name and email
+                    val user: MutableMap<String, String> = HashMap()
+                    user["name"] = auth.currentUser!!.displayName.toString()
+                    user["email"] = auth.currentUser!!.email.toString()
+
+                    db.collection("users")
+                        .document(auth.currentUser!!.email.toString())
+                        .set(user, SetOptions.merge())
+
+                    startActivity(Intent(this, MainActivity::class.java))
+                    finish()
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                }
+            }
+    }
+
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+    }
+}
--- app/src/main/java/com/riskycase/twoVandaH/MainActivity.kt
+++ app/src/main/java/com/riskycase/twoVandaH/MainActivity.kt
@@ -24,6 +24,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.ktx.Firebase
 import com.squareup.picasso.Picasso
 
@@ -33,20 +34,10 @@ class MainActivity: AppCompatActivity() {
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var googleSignInClient: GoogleSignInClient
-    private lateinit var db: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Configure Google Sign In
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.web_client_id))
-            .requestEmail()
-            .build()
-
-        googleSignInClient = GoogleSignIn.getClient(this, gso)
-        // Initialize Firebase Auth
         auth = Firebase.auth
 
         setContentView(R.layout.activity_main)
@@ -70,53 +61,41 @@ class MainActivity: AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
-        db = FirebaseFirestore.getInstance()
-
-        auth.addAuthStateListener { fba ->
-            if(fba.currentUser == null) {
-                navView.menu.findItem(R.id.nav_sign_out).isVisible = false
-                navView.menu.findItem(R.id.nav_sign_in).isVisible = true
-                navView.getHeaderView(0).findViewById<TextView>(R.id.userName).text = getString(R.string.signed_out)
-                navView.getHeaderView(0).findViewById<TextView>(R.id.userMail).text = ""
-                navView.getHeaderView(0).findViewById<ImageView>(R.id.userIcon).setImageResource(R.drawable.default_user)
-                fab.visibility = FloatingActionButton.GONE
-            }
-            else {
-                navView.menu.findItem(R.id.nav_sign_out).isVisible = true
-                navView.menu.findItem(R.id.nav_sign_in).isVisible = false
-                navView.getHeaderView(0).findViewById<TextView>(R.id.userName).text = auth.currentUser!!.displayName
-                navView.getHeaderView(0).findViewById<TextView>(R.id.userMail).text = auth.currentUser!!.email
-                fab.visibility = FloatingActionButton.VISIBLE
-                Picasso.get().load(auth.currentUser!!.photoUrl).into(
-                    navView.getHeaderView(0).findViewById<ImageView>(
-                        R.id.userIcon
-                    )
+        if(auth.currentUser == null) {
+            navView.getHeaderView(0).findViewById<TextView>(R.id.userName).text = getString(R.string.signed_out)
+            navView.getHeaderView(0).findViewById<TextView>(R.id.userMail).text = ""
+            navView.getHeaderView(0).findViewById<ImageView>(R.id.userIcon).setImageResource(R.drawable.default_user)
+        }
+        else {
+            navView.getHeaderView(0).findViewById<TextView>(R.id.userName).text =
+                auth.currentUser!!.displayName
+            navView.getHeaderView(0).findViewById<TextView>(R.id.userMail).text =
+                auth.currentUser!!.email
+            Picasso.get().load(auth.currentUser!!.photoUrl).into(
+                navView.getHeaderView(0).findViewById<ImageView>(
+                    R.id.userIcon
                 )
-                // Create a new user with a name and email
-                val user: MutableMap<String, String> = HashMap()
-                user["name"] = auth.currentUser!!.displayName.toString()
-                user["email"] = auth.currentUser!!.email.toString()
+            )
 
-                db.collection("users")
-                    .document(auth.currentUser!!.email.toString())
-                    .set(user)
-            }
         }
 
         navView.setNavigationItemSelectedListener{ item ->
-            if(item.itemId == R.id.nav_sign_out)
-                auth.signOut()
-            else if (item.itemId == R.id.nav_sign_in)
-                signIn()
-            else if(item.itemId == R.id.nav_available_books){
-                navController.popBackStack()
-                navController.navigate(R.id.nav_available_books)
-                findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
-            }
-            else if(item.itemId == R.id.nav_my_books){
-                navController.popBackStack()
-                navController.navigate(R.id.nav_my_books)
-                findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+            when (item.itemId) {
+                R.id.nav_sign_out -> {
+                    auth.signOut()
+                    startActivity(Intent(this, LaunchActivity::class.java))
+                    finish()
+                }
+                R.id.nav_available_books -> {
+                    navController.popBackStack()
+                    navController.navigate(R.id.nav_available_books)
+                    findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+                }
+                R.id.nav_my_books -> {
+                    navController.popBackStack()
+                    navController.navigate(R.id.nav_my_books)
+                    findViewById<DrawerLayout>(R.id.drawer_layout).closeDrawer(GravityCompat.START)
+                }
             }
             return@setNavigationItemSelectedListener true
         }
@@ -133,47 +112,5 @@ class MainActivity: AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-        if (requestCode == RC_SIGN_IN) {
-            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            try {
-                // Google Sign In was successful, authenticate with Firebase
-                val account = task.getResult(ApiException::class.java)!!
-                Log.d(TAG, "firebaseAuthWithGoogle:" + account.id)
-                firebaseAuthWithGoogle(account.idToken!!)
-            } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
-                Log.w(TAG, "Google sign in failed", e)
-            }
-        }
-    }
-
-    private fun firebaseAuthWithGoogle(idToken: String) {
-        val credential = GoogleAuthProvider.getCredential(idToken, null)
-        auth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d(TAG, "signInWithCredential:success")
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w(TAG, "signInWithCredential:failure", task.exception)
-                }
-            }
-    }
-
-    private fun signIn() {
-        val signInIntent = googleSignInClient.signInIntent
-        startActivityForResult(signInIntent, RC_SIGN_IN)
-    }
-
-    companion object {
-        private const val TAG = "GoogleActivity"
-        private const val RC_SIGN_IN = 9001
     }
 }
